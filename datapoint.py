c3ad6c40a21f49808f4d8fe8b65c39a1e1eeda3f
Marius-Juston <marius.juston@hotmail.fr>
2021-02-06 11:43:31 -0500

    created simple datapoint structure

---
+++ datapoint.py
@@ -0,0 +1,6 @@
+class DataPoint:
+    def __init__(self, data_type, measurement_data, timestamp) -> None:
+        super().__init__()
+        self.data_type = data_type
+        self.measurement_data = measurement_data
+        self.timestamp = timestamp
