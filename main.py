33084f38a7538c5b745dc02245fd18c5e5507a34
Marius-Juston <marius.juston@hotmail.fr>
2021-02-13 22:48:12 -0500

    added sensor distance offset to generated data and UWB sensor

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,6 +12,14 @@ def define_test_data():
 
     ground_truth = []
     sensor_data = []
+    sensor_offset_pose = []
+
+    anchor_pos = np.array([5, 6])
+    anchor_pos2 = np.array([10, 20])
+    # anchor_pos2 = np.array([10,-15])
+    # anchor_pos2 = np.array([1,2])
+
+    sensor_offset = np.array([0, .2])
 
     with open(file, 'r') as file_data:
         for line in file_data.readlines():
@@ -17,58 +27,109 @@ def define_test_data():
 
             sensor_type = line.pop(0)
 
+            noise = np.random.normal(0, .25)
+
             if sensor_type == 'L':
                 val1 = float(line.pop(0))
                 val2 = float(line.pop(0))
                 timestamp = float(line.pop(0)) / 1e6
 
-                sensor_data.append(DataPoint(DataType.LIDAR, np.array([val1, val2]), timestamp))
+                # sensor_data.append(DataPoint(DataType.LIDAR, np.array([val1, val2]), timestamp))
+
+                x = float(line.pop(0))
+                y = float(line.pop(0))
+                vx = float(line.pop(0))
+                vy = float(line.pop(0))
+                yaw = float(line.pop(0))
+
+                pose = np.array([x, y]) + np.matmul(rotation_matrix(yaw), sensor_offset)
+
+                distance = np.linalg.norm(pose - anchor_pos2) + noise
+                sensor_data.append(DataPoint(DataType.UWB, np.array([distance]), timestamp,
+                                             extra={"anchor": anchor_pos2, "sensor_offset": sensor_offset}))
+
             else:
                 line.pop(0)
                 line.pop(0)
                 line.pop(0)
+
                 timestamp = float(line.pop(0)) / 1e6
 
-            x = float(line.pop(0))
-            y = float(line.pop(0))
-            vx = float(line.pop(0))
-            vy = float(line.pop(0))
-            yaw = float(line.pop(0))
+                x = float(line.pop(0))
+                y = float(line.pop(0))
+                vx = float(line.pop(0))
+                vy = float(line.pop(0))
+                yaw = float(line.pop(0))
+
+                pose = np.array([x, y]) + np.matmul(rotation_matrix(yaw), sensor_offset)
+                distance = np.linalg.norm(pose - anchor_pos) + noise
+
+                sensor_data.append(DataPoint(DataType.UWB, np.array([distance]), timestamp,
+                                             extra={"anchor": anchor_pos, "sensor_offset": sensor_offset}))
+
             yaw_rate = float(line.pop(0))
 
             v = np.sqrt(vx ** 2 + vy ** 2)
 
             ground_truth.append(DataPoint(DataType.LIDAR, np.array([x, y, v, yaw, yaw_rate]), timestamp))
+            sensor_offset_pose.append(pose)
 
-    return ground_truth, sensor_data
+    return ground_truth, sensor_data, np.array(sensor_offset_pose)
+
+
+def rotation_matrix(angle):
+    s = np.sin(angle)
+    c = np.cos(angle)
+
+    return [[c, -s], [s, c]]
 
 
 if __name__ == '__main__':
-    ground_truth, sensor_data = define_test_data()
+    ground_truth, sensor_data, sensor_offset_pose = define_test_data()
 
     sensor_std = {
         DataType.LIDAR: {
-            'std': [0.15, 0.15]
+            'std': [0.15, 0.15],
+            'nz': 2
+        },
+        DataType.UWB: {
+            'std': [.25],
+            'nz': 1
         }
     }
 
-    filter = FusionUKF(sensor_std)
+    filter = FusionUKF(sensor_std, .9, 1)
 
     ground_xy = np.array([[g.measurement_data[0], g.measurement_data[1]] for g in ground_truth])
     plt.plot(ground_xy[:, 0], ground_xy[:, 1], color=[1, 0, 0, 1])
+    plt.plot(sensor_offset_pose[:, 0], sensor_offset_pose[:, 1], label="sensor", c='b')
 
-    scatter_xy = np.array([[g.measurement_data[0], g.measurement_data[1]] for g in sensor_data])
-    plt.scatter(scatter_xy[:, 0], scatter_xy[:, 1], color=[0, 0, 1, .5])
+    filter.initialize(ground_truth[0].measurement_data, np.eye(5), ground_truth[0].timestamp)
+    # scatter_xy = np.array([[g.measurement_data[0], g.measurement_data[1]] for g in sensor_data])
+    # plt.scatter(scatter_xy[:, 0], scatter_xy[:, 1], color=[0, 0, 1, .5])
 
     x = []
     y = []
 
+    time_ = []
+
     for sensor in sensor_data:
+        s = time.time_ns()
+
         filter.update(sensor)
+
+        time_.append(time.time_ns() - s)
 
         x.append(filter.x[0])
         y.append(filter.x[1])
 
+    time_ = np.array(time_)
+
+    print(np.mean(time_), np.std(time_), np.sum(time_), len(time_), np.mean(time_[time_ <= 1.4 * 1e7]))
+
     plt.plot(x, y, color=[0, 1, 0, 1])
 
+    plt.show()
+
+    plt.plot(time_)
     plt.show()
