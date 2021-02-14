5b29e9836c1421b30ad5b01c15b25b6522a5c0a1
Marius-Juston <marius.juston@hotmail.fr>
2021-02-13 19:21:38 -0500

    optimized Tc matrix computation

--- state_updater.py
+++ state_updater.py
@@ -24,6 +24,15 @@ class StateUpdater:
 
         return Tc
 
+    def compute_Tc_numpy(self, predicted_x, predicted_z, sigma_x, sigma_z):
+        dx = np.subtract(sigma_x.T, predicted_x).T
+        mask = np.abs(dx[3]) > np.pi
+        dx[3, mask] = dx[3, mask] % (np.pi * 2)
+
+        dz = np.subtract(sigma_z.T, predicted_z)
+
+        return np.matmul(self.WEIGHTS * dx, dz)
+
     def update(self, z, S, Tc, predicted_z, predicted_x, predicted_P):
         Si = np.linalg.inv(S)
         K = np.matmul(Tc, Si)
@@ -35,5 +44,5 @@ class StateUpdater:
         self.nis = np.matmul(dz.transpose(), np.matmul(Si, dz))
 
     def process(self, predicted_x, predicted_z, z, S, predicted_P, sigma_x, sigma_z):
-        Tc = self.compute_Tc(predicted_x, predicted_z, sigma_x, sigma_z)
+        Tc = self.compute_Tc_numpy(predicted_x, predicted_z, sigma_x, sigma_z)
         self.update(z, S, Tc, predicted_z, predicted_x, predicted_P)
